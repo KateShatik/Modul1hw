31ceb2aa458694d0db31abecf0256f5e3d1dd537
KateShatik <katrin.shatik@gmail.com>
2016-10-29 13:38:02 +0300

    My first modul

---
+++ src/FirstClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Катя on 27.10.2016.
+ */
+public class FirstClass {
+    public static void main (String[] arg){
+
+        System.out.println("My first class in JAVA");//jgsa
+    }
+}
---
+++ src/SecondClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Катя on 27.10.2016.
+ */
+public class SecondClass {
+    public static void main (String[] arg){
+        System.out.println("My first class in JAVA");
+    }
+}
